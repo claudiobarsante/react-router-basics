93a86eb23f59fe7e77d3e8bd14deff37b210ab5b
Claudio Barsante <clbmribas@gmail.com>
2024-02-01 16:45:59 -0300

    feat: add <Link/> to HomePage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function HomePage() {
 	return (
 		<>
 			<h1>My Home Page</h1>
+			<p>
+				Go to <Link to='/products'>/products</Link> to see all the products.
+			</p>
 		</>
 	);
 }
