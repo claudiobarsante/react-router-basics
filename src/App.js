fac20890971d893e6799eebac4378897852b9c27
Claudio Barsante <clbmribas@gmail.com>
2024-02-01 16:34:28 -0300

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/Home';
+import ProductsPage from './pages/Products';
+
+//*-- https://example.com.products -> path:'/products'
+
+// -- Alternative way - more similar to older versions of react-router-dom < 6.4.1
+// const routeDefinitions = createRoutesFromElements(
+// 	<Route>
+// 	<Route path='/' element={<HomePage />} />
+// 	<Route path='/products' element={<ProductsPage />} />
+// 	</Route>
+// )
+// -  <RouterProvider router={routeDefinitions} />;
+const router = createBrowserRouter([
+	{ path: '/', element: <HomePage /> },
+	{ path: '/products', element: <ProductsPage /> },
+]);
 function App() {
-  return <div></div>;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
